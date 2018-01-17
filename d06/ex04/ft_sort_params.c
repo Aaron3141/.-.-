0e0f01db939540efb2b772285dca31e2384b29c8
Aaron <aaronhsieh1997@gmail.com>
2018-01-17 13:48:29 -0800

    a

---
+++ d06/ex04/ft_sort_params.c
@@ -0,0 +1,56 @@
+void	ft_putchar(char c);
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_print_param(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[j - 1], argv[j]) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+		ft_print_param(argv[i++]);
+	return (0);
+}
